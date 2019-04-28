4f3bb021029035457b8c73011beef22dbb9b000d
freetes'Mac <freetes@foxmail.com>
2019-04-28 10:27:41 +0800

    update in 4.28

--- src/utils/util.js
+++ src/utils/util.js
@@ -1,33 +1,5 @@
-
-
-function getDate(date){
-  let year = date.getFullYear(), 
-      month = date.getMonth() + 1, 
-      day = date.getDate();
-
-  return `${year}/${month}/${day}`
-}
-
-function getTime(date){
-  let year = date.getFullYear(), 
-      month = date.getMonth() + 1, 
-      day = date.getDate(),
-      hour = date.getHours(),
-      minute = date.getMinutes(),
-      second = date.getSeconds()
-
-  return `${year}/${month}/${day} ${hour}:${minute}:${second}`
-}
-
-function isAfter(date){
-  let now = Date.now()
-  date = new Date(date)
-
-  return now <= date.getTime()
-}
+import moment from 'moment'
 
 module.exports = {
-  getDate,
-  getTime,
-  isAfter
+  moment
 }
